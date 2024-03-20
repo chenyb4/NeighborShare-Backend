9de8550ddceac81cc2f6edb1c8d8b0c596ad2469
Yibing Chen <444980@student.saxion.nl>
2024-03-20 11:17:43 +0100

    credentials service

---
+++ src/services/credentials.js
@@ -0,0 +1,54 @@
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const {StatusCodes} = require("http-status-codes");
+const User = require("../database/models/User");
+
+
+
+const logIn = async (email, password) => {
+    const users = await User.find();
+
+    const user = users.find((user) => {
+        return user.email === email;
+    });
+
+    //if user found, check password. if not, status code unauthorized
+    if (user) {
+        const result = bcrypt.compareSync(password, user.passwordHash);
+        if (result) {
+            //user is authenticated, send a token
+            return jwt.sign({
+                name: user.name,
+                email: user.email
+            }, user.secret);
+        }
+    }
+    return false;
+};
+
+
+exports.auth=(req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    console.log("auth funtion called")
+
+    if(email && password) {
+
+        logIn(email, password)
+            .then(token => {
+                if (token) {
+                    return res.status(StatusCodes.ACCEPTED).send({token});
+                } else {
+                    return res.status(StatusCodes.UNAUTHORIZED).send('Credentials incorrect!');
+                }
+            })
+            .catch(error => {
+                return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error.message);
+            });
+
+    } else {
+        return res.status(StatusCodes.BAD_REQUEST).send('Error in email or password!');
+    }
+}
